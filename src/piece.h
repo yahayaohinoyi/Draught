0010cb16c65dac3d27870cb95975e9a6a9059faf
yahayaohinoyi <yahaya.suleiman2204@gmail.com>
2022-02-07 06:01:10 +0000

    BUG: circular dependency of header files when creating class entities

---
+++ src/piece.h
@@ -0,0 +1,31 @@
+//
+// Created by MAC on 07/02/2022.
+//
+
+#ifndef DRAUGHT_PIECE_H
+#define DRAUGHT_PIECE_H
+
+#include <iostream>
+#include <vector>
+#include "position.h"
+
+class Position;
+
+namespace Draught
+{
+    class Piece
+    {
+        public:
+            enum PieceType
+            {
+                KING, PAWN
+            };
+        private:
+            const std::string pieceId;
+            Position pos;
+//            std::vector<Position> moves;
+            PieceType type;
+        };
+}
+
+#endif //DRAUGHT_PIECE_H
---
+++ src/position.h
@@ -0,0 +1,23 @@
+//
+// Created by MAC on 07/02/2022.
+//
+
+#ifndef DRAUGHT_POSITION_H
+#define DRAUGHT_POSITION_H
+
+#include <iostream>
+#include "piece.h"
+
+class Piece;
+
+namespace Draught
+{
+    class Position
+    {
+    private:
+        const std::string pos;
+//        Piece p;
+    };
+}
+
+#endif //DRAUGHT_POSITION_H
